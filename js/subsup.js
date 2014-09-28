7e2f8b4645f99c03a16f6ed260f388453e5d70ce
insolita <donnainsolita@gmail.com>
2014-09-29 07:14:18 +0900

    subsup finished

--- js/subsup.js
+++ js/subsup.js
@@ -1,3 +1,36 @@
-/**
- * Created by lusik on 29.09.14.
+/*
+ add subscript and superscript buttons
+ (c)Insolita
  */
+if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
+RedactorPlugins.subsup = function()
+{
+	return {
+		init: function ()
+		{
+			this.opts.langs['ru'] = $.extend({
+				subname: 'Подстрочный индекс',
+				supname: 'Надстрочный индекс'
+			}, this.opts.langs['ru']);
+			this.opts.langs['en'] = $.extend({
+				subname: 'Subscript',
+				supname: 'Superscript'
+			}, this.opts.langs['en']);
+			this.opts.curLang = this.opts.langs[this.opts.lang];
+			var sub_button = this.button.add('subscript', this.opts.curLang.subname);
+			var sup_button = this.button.add('superscript', this.opts.curLang.supname);
+			this.button.setAwesome('subscript', 'fa-subscript');
+			this.button.setAwesome('superscript', 'fa-supercript');
+			this.button.addCallback(sub_button, this.subsup.subscripter);
+			this.button.addCallback(sup_button, this.subsup.supscripter);
+		},
+		subscripter: function()
+		{
+			this.inline.format('sub');
+		},
+		supscripter: function()
+		{
+			this.inline.format('sup');
+		}
+	};
+};
