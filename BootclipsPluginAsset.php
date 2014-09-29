987aa6806a67eca3756d02b855f9a49559ad0f96
Insolita <Insolita@users.noreply.github.com>
2014-09-29 12:03:42 +0900

    Update BootclipsPluginAsset.php

--- BootclipsPluginAsset.php
+++ BootclipsPluginAsset.php
@@ -16,7 +16,7 @@ class BootclipsPluginAsset extends AssetBundle
     /**
      * @inheritdoc
      */
-    public $sourcePath = '@vendor/insolita/imperavi-plugins/js';
+    public $sourcePath = '@vendor/insolita/extimperavi/js';
     public $js
         = [
             'bootclips/bootclips.js'
